9cb8c667d73b9a8a2d26f826cc30d55fb5006717
Xinran Che <gracexinran@gmail.com>
2019-04-17 01:23:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,18 +34,30 @@ function johnLennonFacts(facts){
   return array;
 }
 
-function iLoveTheBeatles(p){
-  var array = [];
-  var i=0;
-    if(p === 7){
-      do{
-        array.push("I love the Beatles!");
-        i++;
-      }
-      while(i<8)
-    }
-    else if(p === 17){
-      array.push("I love the Beatles!");
-    }
-  return array;
+// function iLoveTheBeatles(p){
+//   var array = [];
+//   var i=0;
+//     if(p === 7){
+//       do{
+//         array.push("I love the Beatles!");
+//         i++;
+//       }
+//       while(i<8)
+//     }
+//     else if(p === 17){
+//       array.push("I love the Beatles!");
+//     }
+//   return array;
+// }
+
+function iLoveTheBeatles(number){
+  if(number === 7){
+    var array = [];
+    for(var i=0; i<8; i++){
+      array.push('I love the Beatles!');
+    };
+    return array;
+  }else if(number === 17){
+    return "I love the Beatles!"
+  };
 }
