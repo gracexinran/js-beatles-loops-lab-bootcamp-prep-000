089416e5f3681ee65c7606661a5a9f3529b160f9
Xinran Che <gracexinran@gmail.com>
2019-04-17 00:52:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 // add solution here
+// 
+// function theBeatlesPlay(musicians,instruments){
+//   var array = [];
+//   var i;
+//   for(i=0; i<musicians.length; i++){
+//     array.push(musicians[i] +' plays ' + instruments[i])
+//   };
+//   return array;
+// }
 
 function theBeatlesPlay(musicians,instruments){
   var array = [];
-  var i;
-  for(i=0; i<musicians.length; i++){
-    array.push(musicians[i] +' plays ' + instruments[i])
+  for(var i=0; i<musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
   };
-  return array;
 }
 
 function johnLennonFacts(fact){
